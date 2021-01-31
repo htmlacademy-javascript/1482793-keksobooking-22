4e45775d041b5c9071242885e26a3084057f5bef
Мария Громова <gromova.marija.i@gmail.com>
2021-01-31 16:04:45 +0300

    Добавляет утилитарные функции

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Функция, возвращающая случайное целое число из переданного диапазона включительно (задание выполнено с использованием материалов сайта learn.javascript.ru и книги "JS для детей")
 const getRandomInteger = (min, max) => {
-  if (min >= 0 && max > min) {
+  if (min >= 0 && max >= min) {
     return Math.floor(min + Math.random() * (max - min));
   }
   return;
@@ -12,7 +12,7 @@ getRandomInteger(2, 30);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно (задание выполнено с использованием материалов сайта learn.javascript.ru)
 const getRandomNumber = (min, max, digitsAfterComma) => {
-  if (min >= 0 && max > min) {
+  if (min >= 0 && max >= min) {
     return +(min + Math.random() * (max - min)).toFixed(digitsAfterComma);
   }
   return;
