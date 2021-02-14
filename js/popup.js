a4c213c591e62931748de13d93feb8c356087fe8
Мария Громова <gromova.marija.i@gmail.com>
2021-02-15 01:10:05 +0300

    Исправляет ошибки в коде для генерации похожих объявлений

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,16 @@
 import {createSimilarAds} from './data.js';
 
-const MAP_CANVAS = document.querySelector('.map__canvas');
+const MAP_CANVAS = document.querySelector('#map-canvas');
 
 const SIMILAR_CARD_TEMPLATE = document.querySelector('#card').content.querySelector('.popup');
 
 const SIMILAR_ADS = createSimilarAds();
 
-// Функция, возвращающая русский текст для типа объекта размещения
-const getAccommodationType = (type) => {
-  switch (type) {
-    case 'flat':
-      return 'Квартира';
-    case 'bungalow':
-      return 'Бунгало';
-    case 'house':
-      return 'Дом';
-    case 'palace':
-      return 'Дворец';
-  }
+const TYPE_DICTIONARY = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
 };
 
 // Функция, возвращаюшая список удобств объекта размещения в виде элементов HTML-списка
@@ -42,28 +35,39 @@ const createPhotosList = (photos, template) => {
     PHOTOS_FRAGMENT.appendChild(NEW_ELEMENT);
   })
   return PHOTOS_FRAGMENT;
-}
+};
+
+// Функция, определяющая окончание слова в зависимости от числительного (подсмотрела решение здесь - https://realadmin.ru/coding/sklonenie-na-javascript.html)
+const declineWord = (number, words) => {
+  return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+};
 
 const createSimilarCard = ({author, offer}) => {
   const SIMILAR_CARD_ELEMENT = SIMILAR_CARD_TEMPLATE.cloneNode(true);
   SIMILAR_CARD_ELEMENT.querySelector('.popup__title').textContent = offer.title;
   SIMILAR_CARD_ELEMENT.querySelector('.popup__text--address').textContent = offer.address;
   SIMILAR_CARD_ELEMENT.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  SIMILAR_CARD_ELEMENT.querySelector('.popup__type').textContent = getAccommodationType(offer.type);
-  SIMILAR_CARD_ELEMENT.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  SIMILAR_CARD_ELEMENT.querySelector('.popup__type').textContent = TYPE_DICTIONARY[offer.type];
+  SIMILAR_CARD_ELEMENT.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${declineWord(offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${offer.guests} ${declineWord(offer.rooms, ['гостя', 'гостей', 'гостей'])}`;
   SIMILAR_CARD_ELEMENT.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
   const CARD_FEATURES = SIMILAR_CARD_ELEMENT.querySelector('.popup__features');
   CARD_FEATURES.innerHTML = '';
   CARD_FEATURES.appendChild(createFeaturesList(offer.features));
   SIMILAR_CARD_ELEMENT.querySelector('.popup__description').textContent = offer.description;
+
   const CARD_PHOTOS = SIMILAR_CARD_ELEMENT.querySelector('.popup__photos');
   const CARD_PHOTO_TEMPLATE = SIMILAR_CARD_ELEMENT.querySelector('.popup__photo');
   CARD_PHOTOS.innerHTML = '';
   CARD_PHOTOS.appendChild(createPhotosList(offer.photos, CARD_PHOTO_TEMPLATE));
-  SIMILAR_CARD_ELEMENT.querySelector('.popup__description').src = author.avatar;
-  MAP_CANVAS.appendChild(SIMILAR_CARD_ELEMENT);
+  SIMILAR_CARD_ELEMENT.querySelector('.popup__avatar').src = author.avatar;
+
   return SIMILAR_CARD_ELEMENT;
 };
 
-const SIMILAR_CARD = createSimilarCard(SIMILAR_ADS[0]);
-MAP_CANVAS.appendChild(SIMILAR_CARD);
+const SIMILAR_CARDS = SIMILAR_ADS.map(createSimilarCard);
+const CARDS_FRAGMENT = document.createDocumentFragment();
+// SIMILAR_CARDS.forEach(item => CARDS_FRAGMENT.appendChild(item));
+CARDS_FRAGMENT.appendChild(SIMILAR_CARDS[0]);
+MAP_CANVAS.appendChild(CARDS_FRAGMENT);
+
