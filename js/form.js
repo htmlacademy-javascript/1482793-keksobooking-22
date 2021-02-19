335f30df9869c0953bd176e79cceced54d5c99d6
Мария Громова <gromova.marija.i@gmail.com>
2021-02-19 19:19:31 +0300

    Добавляет валидацию формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,7 @@
 const AD_FORM = document.querySelector('.ad-form');
 
+const TITLE = AD_FORM.querySelector('#title');
+
 const TYPE = AD_FORM.querySelector('#type');
 
 const PRICE = AD_FORM.querySelector('#price');
@@ -12,11 +14,22 @@ const FIELDSETS = AD_FORM.querySelectorAll('fieldset');
 
 const ADDRESS = AD_FORM.querySelector('#address');
 
+const ROOM_NUMBER = AD_FORM.querySelector('#room_number');
+
+const CAPACITY = AD_FORM.querySelector('#capacity');
+
 const ACCOMODATION_PRICE = {
   bungalow: 0,
   flat: 1000,
   house: 5000,
   palace: 10000,
+};
+
+const ROOMS_CAPACITY = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
 };
 
 const disableForm = () => {
@@ -35,12 +48,49 @@ const enableForm = () => {
 
 disableForm();
 
+TITLE.addEventListener('invalid', () => {
+  if (TITLE.validity.tooShort) {
+    TITLE.setCustomValidity(`Минимальное количество символов: ${TITLE.minLength}`);
+  } else if (TITLE.validity.tooLong) {
+    TITLE.setCustomValidity(`Максимальное количество символов: ${TITLE.maxLength}`);
+  } else if (TITLE.validity.valueMissing) {
+    TITLE.setCustomValidity('Обязательное поле');
+  } else {
+    TITLE.setCustomValidity('');
+  }
+});
+
 ADDRESS.readOnly = true;
 
 TYPE.addEventListener('change', () => {
   const MIN_PRICE = ACCOMODATION_PRICE[TYPE.value];
   PRICE.min = MIN_PRICE;
   PRICE.placeholder = MIN_PRICE;
+});
+
+PRICE.addEventListener('invalid', () => {
+  if (PRICE.validity.rangeUnderflow) {
+    PRICE.setCustomValidity(`Минимальная цена: ${PRICE.min}`);
+  } else if (PRICE.validity.tooLong) {
+    TITLE.setCustomValidity(`Максимальная цена: ${PRICE.max}`);
+  } else if (PRICE.validity.valueMissing) {
+    PRICE.setCustomValidity('Обязательное поле');
+  } else {
+    PRICE.setCustomValidity('');
+  }
+});
+
+const getRoomCapacity = () => {
+  for (let option of CAPACITY.options) {
+    option.disabled = ROOMS_CAPACITY[ROOM_NUMBER.value].includes(option.value) ? false : true;
+  }
+  CAPACITY.value = ROOMS_CAPACITY[ROOM_NUMBER.value].includes(CAPACITY.value) ? CAPACITY.value : ROOMS_CAPACITY[ROOM_NUMBER.value][0];
+};
+
+getRoomCapacity();
+
+ROOM_NUMBER.addEventListener('change', () => {
+  getRoomCapacity();
 });
 
 CHECK_IN_TIME.addEventListener('change', () => {
